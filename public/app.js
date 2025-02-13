b567077ca277acb513be80119a2c68f81a174be0
megustaelcampo <pablobejarano2005@gmail.com>
2025-02-13 14:22:06 +0100

    Primera instancia de las pantallas

--- public/app.js
+++ public/app.js
@@ -1,19 +1,28 @@
 let app = new Vue({
     el: '#app',
     data: {
-        mensaje: '¡Estás Logeado!',
         loggedIn: false,
-        email: '',
-        password: ''
+        isAdmin: 'false',
+        email_input: '',
+        password_input: '',
+        usuario: "",            // Variable para guardar el usuario logeado
+        pantalla: 'inicio',     // Variable para controlar la pantalla que se muestra
+        lista_usuarios: []      // Variable para guardar la lista de usuarios
     },
     methods: {
         login() {
             fetch('http://localhost/Proyecto_JS_2/private/apiGrua/public/usuarios')
                 .then(response => response.json())
                 .then(users => {
-                    const user = users.find(user => user.email === this.email && user.password === this.password);
+                    const user = users.find(user => user.email === this.email_input && user.password === this.password_input);
                     if (user) {
                         this.loggedIn = true;
+                        this.usuario = user;
+                        if(user.admin == 'admin'){
+                            this.isAdmin = 'true';
+                        }else{
+                            this.isAdmin = 'false';
+                        }
                     } else {
                         alert('Invalid credentials');
                     }
@@ -22,6 +31,33 @@ let app = new Vue({
                     console.error('Error fetching users:', error);
                     alert('Error logging in. Please try again later.');
                 });
-        }
+        },
+        logout() {
+            this.loggedIn = false;
+            this.isAdmin = 'false',
+            this.email_input = '';
+            this.password_input = '';
+        },
+        pantallaListaVehiculos() {
+            cargarListaUsuarios();
+            this.pantalla = 'lista.vehiculo';
+        },
+        cargarListaUsuarios() {
+            fetch('http://localhost/Proyecto_JS_2/private/apiGrua/public/usuarios')
+                .then(response => response.json())
+                .then(users => {
+                    this.lista_usuarios = users;
+                })
+                .catch(error => {
+                    console.error('Error fetching users:', error);
+                    alert('Error fetching users. Please try again later.');
+                });
+        },
+        pantallaListaRetiradas() {
+            this.pantalla = 'lista.retiradas';
+        },
+        pantallaUsuarios() {
+            this.pantalla = 'usuarios';
+        },
     }
 });
